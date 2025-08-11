87ea3dd70e40675fd383f2fd518fff7283227700
19013na <19013@naver.com>
2025-08-11 10:27:21 +0900

    MNMS-282 fix: 예매자 dto 변경

--- src/main/java/com/mnms/booking/dto/response/UserInfoResponseDTO.java
+++ src/main/java/com/mnms/booking/dto/response/UserInfoResponseDTO.java
@@ -3,11 +3,13 @@ package com.mnms.booking.dto.response;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @Builder
 public class UserInfoResponseDTO {
     private String phone;
     private String email;
-    private String address;
+    private List<AddressResponseDTO> address;
     private String birth;
 }
